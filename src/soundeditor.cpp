2f8e676013e3f928494f3fe395e3b097240a1bfd
Rohan Hill <rohan@synthstrom.com>
2023-11-08 01:34:03 +1300

    V4.1.4-beta2

--- src/soundeditor.cpp
+++ src/soundeditor.cpp
@@ -2123,7 +2123,7 @@ MenuItemSubmenu coloursSubmenu;
 
 
 
-char const* firmwareString = "4.1.4-beta1";
+char const* firmwareString = "4.1.4-beta2";
 
 class MenuItemFirmwareVersion final : public MenuItem {
 public:
